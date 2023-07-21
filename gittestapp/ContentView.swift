98171b362a69e4a599cebfbf0b3d4916f1f171d4
Jago Goddard <jago@pullscription.com>
2023-07-21 10:24:10 -0700

    Initial Commit

---
+++ gittestapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gittestapp
+//
+//  Created by Jago Lourenco-Goddard on 7/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gittestapp/gittestappApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestappApp.swift
+//  gittestapp
+//
+//  Created by Jago Lourenco-Goddard on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct gittestappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
